944922ae116df24b1abaddc002c99ddd2529d911
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2021-02-06 11:47:31 +0530

    A few corrections

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,7 @@ import { setUrl } from '../../context/news/news.action.creators';
 
 const Header = () => {
     const [{ url }, dispatch] = useNewsContext();
-    const URL_TESLA = 'http://newsapi.org/v2/everything?q=tesla&from=2021-01-04&sortBy=publishedAt&apiKey=';
+    const URL_TESLA = 'http://newsapi.org/v2/everything?q=tesla&from=2021-01-06&sortBy=publishedAt&apiKey=';
     const URL_BUSINESS = 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=';
     const URL_APPLE = 'http://newsapi.org/v2/everything?q=apple&from=2021-02-02&to=2021-02-02&sortBy=popularity&apiKey=';
     const URL_TECH = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=';
